df6719918561d38a0949dd227251147a25c48891
Sedin-Mulasalihovic <codexc544@gmail.com>
2025-08-06 02:52:30 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+// Wenn Bot startet
+client.once('ready', () => {
+  console.log(`Eingeloggt als ${client.user.tag}!`);
+});
+
+// Wenn eine Nachricht gesendet wird
+client.on('messageCreate', message => {
+  if(message.author.bot) return; // Ignoriere Bots
+
+  if(message.content.toLowerCase() === '!hallo') {
+    message.channel.send('Hallo! Ich bin dein Bot 🤖');
+  }
+});
+
+// Bot mit Token einloggen
+client.login('MTQwMjQ0MzI5NzkxNzYzNjYyOA.GN_Vtc.N9u0Z6CEOhCRWMTXgtoj4szdFosHSMg5rBQk4o');
